01f68ae2eaff09dfc5ee8e8e3e79042bb21f0bcf
Fleitas Mathias <matu.cunha@gmail.com>
2023-09-30 18:41:04 -0300

    Proyacto Pre entrega 1

---
+++ src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () =>{
+    return(
+        <nav>
+            <h1>MF Tecnologia</h1>
+         <ul>
+            <li>Mather</li>
+            <li>Procesador</li>
+            <li>Acesorios</li>
+            <li>Contacto</li>
+         </ul>
+        </nav>
+    )
+}
+
+export default Navbar
