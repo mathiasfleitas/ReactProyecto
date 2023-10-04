818f5f39f2150756b23b7d2a32197322f739668c
Fleitas Mathias <matu.cunha@gmail.com>
2023-10-03 21:52:41 -0300

    Proyecto Pre-Entrega 1

--- src/componentes/CartWidget/CartWidget.jsx
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -1,9 +1,9 @@
-
+import style from './Car.module.css'
 
 const CartWidget = () =>{
     return(
         <div>
-            <img src="../../../img/carrito.png" alt=""/>
+            <img src="../../../img/carrito.png" alt="" className={style.img}/>
         </div>
     )
 
--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
 import CartWidget from "../CartWidget/CartWidget"
+import style from './Nav.module.css'
 
 const Navbar = () =>{
     return(
-        <nav>
-            <h1>MF Tecnologia</h1>
-         <ul>
+        <nav className={style.nav}>
+            <h1 className={style.titulo}>MF Tecnologia</h1>
+         <ul className={style.display}>
             <li>
-                <a href="">Mobo</a>
+                <a href="" className={style.color}>Motherboard</a>
             </li>
             <li>
-                <a href="">Procesador</a>
+                <a href="" className={style.color}>Procesador</a>
             </li>
             <li>
-                <a href="">Accesorios</a>
+                <a href="" className={style.color}>Accesorios</a>
             </li>
             <li>
-                <a href="">Contacto</a>
+                <a href="" className={style.color}>Contacto</a>
             </li>
+            <CartWidget/>
          </ul>
-         <CartWidget/>
+         
         </nav>
     )
 }
